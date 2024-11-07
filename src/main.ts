56570f06981dea0fac88043e85eff1272e793063
Emil Sabev <emozconfidence@gmail.com>
2024-11-06 21:43:08 -0800

    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const button = document.createElement("button");
+button.innerHTML = "button";
+app.appendChild(button);
+
+button.addEventListener("click", () => alert("you clicked the button!"));
