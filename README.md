43be55902aaccd462e18bfa8a530a337b1db0b32
Emil Sabev <emozconfidence@gmail.com>
2024-11-09 07:18:27 -0800

    D3.b complete

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # cmpm-121-demo-3
 
-D3.1 changes/notes:
+D3.a changes/notes:
 -chatgpt helped with popups: https://chatgpt.com/share/672d5eee-d078-8001-9629-544be7e2ba1f
 -chatgpt helped a bit with Cache interface: https://chatgpt.com/share/672d6a83-8f00-8001-b28a-53e15fb27f5a
+
+D3.b changes/notes:
+-used chatgpt to help with flyweight: https://chatgpt.com/share/672f6ccb-64ac-8001-88d9-2d98aff561e2
+Instead of creating a board file, im creating the flyweight pattern in one file because it fits my design better
+-used chatgpt to help with coin formatting: https://chatgpt.com/share/672f773c-af48-8001-b658-bb1b9643242f
--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,50 @@ import "./style.css";
 import "./leafletWorkaround.ts";
 import luck from "./luck.ts";
 
+interface Coin {
+  i: number;
+  j: number;
+  serial: number;
+}
+
 interface Cache {
-  row: number;
-  col: number;
+  i: number;
+  j: number;
+  coins: Coin[];
+}
+
+class FlyweightCacheFactory {
+  private cacheMap: Map<string, Cache> = new Map();
+
+  public getCache(i: number, j: number): Cache {
+    const key = `${i}, ${j}`;
+    if (!this.cacheMap.has(key)) {
+      const newCache: Cache = {
+        i: i * GCS_CONVERSION,
+        j: j * GCS_CONVERSION,
+        coins: [],
+      };
+
+      this.cacheMap.set(key, newCache);
+    }
+
+    return this.cacheMap.get(key)!;
+  }
 }
 
 const cacheArray: Cache[] = [];
+const playerCoins: Coin[] = [];
 
-const CLASSROOM_LOCATION = leaflet.latLng(
-  36.98949379578401,
-  -122.06277128548504,
-);
+const NULL_ISLAND = leaflet.latLng(0, 0);
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_SIZE = 0.0001;
 const GRID_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
 const MARKER_RADIUS = 15;
+const GCS_CONVERSION = 1000;
 
 const map = leaflet.map(document.getElementById("map")!, {
-  center: CLASSROOM_LOCATION,
+  center: NULL_ISLAND,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   zoomControl: false,
   scrollWheelZoom: false,
@@ -34,14 +59,13 @@ leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-leaflet.circleMarker(CLASSROOM_LOCATION, {
+leaflet.circleMarker(NULL_ISLAND, {
   radius: MARKER_RADIUS,
   color: "red",
 }).bindTooltip("You are here!").addTo(map);
 
-let playerCoins = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "Coins Collected: 0";
+statusPanel.innerHTML = "Coins Collected:";
 
 generateCaches();
 displayCaches();
@@ -55,12 +79,22 @@ function spawnCache(cache: Cache) {
 }
 
 function createCachePopup(rect: leaflet.rectangle, cache: Cache) {
-  let coinAmount = Math.floor(
-    luck([cache.row, cache.col, "initialVal"].toString()) * 10,
+  const coinAmount = Math.floor(
+    luck([cache.i, cache.j, "initialVal"].toString()) * 10,
   );
 
+  let coinString: string = "";
+
+  for (let serial = 0; serial < coinAmount; serial += 1) {
+    const newCoin: Coin = { i: cache.i, j: cache.j, serial };
+    cache.coins.push(newCoin);
+
+    coinString += newCoin.i.toString() + ":" + newCoin.j.toString() + "#" +
+      newCoin.serial + "<br>";
+  }
+
   const popupContent =
-    `Cache Location: (${cache.row}, ${cache.col})<br>Coin Amount: ${coinAmount}<br>
+    `Cache Location: (${cache.i}, ${cache.j})<br>Coin Amount: ${coinAmount}<br><br>${coinString}<br>
     <button id="collect">collect</button><br><button id="deposit">deposit</button><br>`;
 
   rect.bindPopup(popupContent);
@@ -70,11 +104,12 @@ function createCachePopup(rect: leaflet.rectangle, cache: Cache) {
 
     if (collectButton) {
       collectButton.addEventListener("click", function () {
-        if (coinAmount > 0) {
-          coinAmount -= 1;
-          playerCoins += 1;
-
-          updateDisplay(cache, coinAmount, playerCoins, rect);
+        if (cache.coins.length > 0) {
+          const coin = cache.coins.pop();
+          if (coin) {
+            playerCoins.push(coin);
+          }
+          updateDisplay(cache, playerCoins, rect);
         }
       });
     }
@@ -83,11 +118,13 @@ function createCachePopup(rect: leaflet.rectangle, cache: Cache) {
 
     if (depositButton) {
       depositButton.addEventListener("click", function () {
-        if (playerCoins > 0) {
-          coinAmount += 1;
-          playerCoins -= 1;
+        if (playerCoins.length > 0) {
+          const coin = playerCoins.pop();
+          if (coin) {
+            cache.coins.push(coin);
+          }
 
-          updateDisplay(cache, coinAmount, playerCoins, rect);
+          updateDisplay(cache, playerCoins, rect);
         }
       });
     }
@@ -96,24 +133,32 @@ function createCachePopup(rect: leaflet.rectangle, cache: Cache) {
 
 function updateDisplay(
   cache: Cache,
-  coinAmount: number,
-  playerCoins: number,
+  playerCoins: Coin[],
   rect: leaflet.rectangle,
 ) {
-  statusPanel.innerHTML = `Coins Collected: ${playerCoins}`;
+  const playerCoinStrings = playerCoins.map((coin) =>
+    `${coin.i}:${coin.j}#${coin.serial}`
+  );
+  const playerCoinDisplay = playerCoinStrings.join(", ");
+  statusPanel.innerHTML = `Coins Collected:<br>${playerCoinDisplay}`;
+
+  const cacheCoinString = cache.coins.map((coin) =>
+    `${coin.i}:${coin.j}#${coin.serial}`
+  );
+  const cacheCoinDisplay = cacheCoinString.join("<br>");
 
   const popupContent =
-    `Cache Location: (${cache.row}, ${cache.col})<br>Coin Amount: ${coinAmount}<br>
+    `Cache Location: (${cache.i}, ${cache.j})<br>Coin Amount: ${cache.coins.length}<br><br>${cacheCoinDisplay}<br>
     <button id="collect">collect</button><br><button id="deposit">deposit</button><br>`;
 
   rect.getPopup()?.setContent(popupContent);
 }
 
 function getCellBounds(cache: Cache) {
-  const origin = CLASSROOM_LOCATION;
+  const origin = NULL_ISLAND;
 
-  const centerLat = origin.lat + (cache.row + 0.5) * TILE_SIZE;
-  const centerLng = origin.lng + (cache.col + 0.5) * TILE_SIZE;
+  const centerLat = (origin.lat + (cache.i + 0.5) * TILE_SIZE) / GCS_CONVERSION;
+  const centerLng = (origin.lng + (cache.j + 0.5) * TILE_SIZE) / GCS_CONVERSION;
 
   const northEastCorner = leaflet.latLng(
     centerLat + (TILE_SIZE / 2),
@@ -128,10 +173,13 @@ function getCellBounds(cache: Cache) {
 }
 
 function generateCaches() {
-  for (let row = -GRID_SIZE; row < GRID_SIZE; row++) {
-    for (let col = -GRID_SIZE; col < GRID_SIZE; col++) {
-      if (luck([row, col].toString()) < CACHE_SPAWN_PROBABILITY) {
-        cacheArray.push({ row, col });
+  const cachefactory = new FlyweightCacheFactory();
+  for (let i = -GRID_SIZE; i < GRID_SIZE; i++) {
+    for (let j = -GRID_SIZE; j < GRID_SIZE; j++) {
+      if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+        const cache = cachefactory.getCache(i, j);
+
+        cacheArray.push(cache);
       }
     }
   }
